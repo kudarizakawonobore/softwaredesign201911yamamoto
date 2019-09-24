1b066583ba62f4315b3c2202446895a4d29be384
t.yamamoto <kudarizakawonobore@gmail.com>
2019-09-24 17:35:11 +0000

    add log

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('-----------------------------')
+  console.log('OK')
+  console.log('-----------------------------')
   res.render('index', { title: 'Express' });
 });
 
 router.get("/api/error", (req, res, next) => {
+  console.log('-----------------------------')
+  console.error('ERROR')
+  console.log('-----------------------------')
     const err = new Error("例外が発生しました")
     throw err
 });
